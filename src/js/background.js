924e5a1d1db5529e0be286bca4c7963c74e700f3
Jerry <jerry.fanelli@gmail.com>
2023-12-06 13:55:22 +0100

    Implement base64 postMessage

--- src/js/background.js
+++ src/js/background.js
@@ -1,19 +1,9 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.active) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        func: () => console.log("Inscription Helper Loaded...")
-      });
-    }
-  });
-  
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.request) {
-        // Perform the requested action, e.g., an HTTP request
-        fetch(message.request.url)
-            .then(response => response.json())
-            .then(data => sendResponse({data: data}))
-            .catch(error => sendResponse({error: error}));
-        return true; // Indicates that the response is sent asynchronously
+        // Execute your content script instead of just logging a message
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ['content.js'] // Ensure this path is correct
+        });
     }
 });
--- src/js/content.js
+++ src/js/content.js
@@ -1,10 +1,9 @@
-function replaceIPFSProtocol(node) {
-    chrome.storage.local.get('ipfsGateway', function(data) {
-        const gateway = data.ipfsGateway || 'https://ipfs.io/ipfs/'; // Default gateway
+async function replaceIPFSProtocol(node) {
+    try {
+        const data = await chrome.storage.local.get('ipfsGateway');
+        const gateway = data.ipfsGateway || 'https://ipfs.io/ipfs/';
         
-        // Check if the node is an element node and has attributes
         if (node.nodeType === Node.ELEMENT_NODE && node.hasAttributes()) {
-            // Get all attributes of the node
             const attributes = node.attributes;
             for (let attr of attributes) {
                 if (attr.value.includes('ipfs://')) {
@@ -12,17 +11,82 @@ function replaceIPFSProtocol(node) {
                 }
             }
         }
-    })
+    } catch (error) {
+        console.error('Error in replaceIPFSProtocol:', error);
+    }
 }
 
-// Use the existing handleMutations function
 function handleMutations(mutations) {
     for (let mutation of mutations) {
         for (let node of mutation.addedNodes) {
-            replaceIPFSProtocol(node); // Existing function for IPFS
+            // Existing IPFS protocol replacement
+            replaceIPFSProtocol(node);
+
+            // Check for the addition of the inscribe button or its container
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                if (node.id === 'inscribe-button' || node.querySelector('#inscribe-button')) {
+                    setupInscribeButtonListener();
+                }
+            }
         }
     }
 }
+
+function setupInscribeButtonListener() {
+    const inscribeButton = document.getElementById('inscribe-button');
+    if (inscribeButton) {
+        console.log('Inscribe button found:', inscribeButton);
+        inscribeButton.addEventListener('click', handleInscribeClick);
+    } else {
+        console.log('Inscribe button not found');
+    }
+}
+
+function handleInscribeClick() {
+    console.log('Inscribe clicked');
+    const modalImageSrc = document.querySelector('#modal-image-container img').src;
+
+    // Create a new Image object
+    const image = new Image();
+    image.crossOrigin = 'anonymous'; // Set cross-origin to anonymous
+    image.onload = function() {
+        // Image is loaded, now we can convert it to base64
+        convertImageToBase64(image)
+            .then(base64data => {
+                // Inject the base64 data into the page
+                window.postMessage({
+                    type: 'SET_BASE64_DATA',
+                    base64data: base64data
+                }, '*');
+
+                console.log('window.currentBase64data has been set in the page context.');
+            })
+            .catch(error => {
+                console.error('Error converting image to base64:', error);
+            });
+    };
+    image.onerror = function() {
+        console.error('Error loading image with CORS enabled.');
+    };
+    // Set the source to the image to trigger the load
+    image.src = modalImageSrc;
+}
+
+async function convertImageToBase64(imgElement) {
+    return new Promise((resolve, reject) => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = imgElement.naturalWidth || imgElement.width;
+        canvas.height = imgElement.naturalHeight || imgElement.height;
+        try {
+            ctx.drawImage(imgElement, 0, 0, canvas.width, canvas.height);
+            resolve(canvas.toDataURL());
+        } catch (error) {
+            reject(error); // Reject the promise if drawing the image fails
+        }
+    });
+}
+
 
 // Existing code to inject script and create observer
 var s = document.createElement('script');
